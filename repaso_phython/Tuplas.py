1e68946ec3223d724a7f21e1414952b16db38729
andrearendon26 <andrea20rendon@gmail.com>
2023-03-09 08:16:30 -0500

    Se crea el archivo Tuplas

---
+++ repaso_phython/Tuplas.py
@@ -0,0 +1,24 @@
+# Valores numéricos
+
+numeros = (4, 17, 39, 12)
+print(numeros)
+
+# Valores Booleanos
+
+booleanos = (False, False, True, False)
+print(booleanos)
+
+#  Mezcla de diferentes tipos de datos
+
+persona = ("Persona de ejemplo", "Max", 1974, True)
+print(persona)
+
+# Seleccionar  elementos dentro de una tupla
+
+animales = ("perro", "gato", "ratón")
+print(animales[1])
+
+# Determinar la longitud de una tupla
+
+animales = ("perro", "gato", "ratón", "serpiente", "caballo")
+print(len(animales))
