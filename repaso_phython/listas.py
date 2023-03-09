6869afad38bbbbf8075e850e28429bbca0594771
andrearendon26 <andrea20rendon@gmail.com>
2023-03-09 08:08:17 -0500

    Se crea el archivo Listas

---
+++ repaso_phython/listas.py
@@ -0,0 +1,29 @@
+# Una lista de numeros enteros
+
+numeros = [1,2,3,4,5]
+print(numeros)
+
+# una lista de cadena de caracteres
+
+nombres = ['Ana', 'Juan','Damiel','Maria']
+print(nombres)
+
+# Una lista que contiene diferentes tipos de datos
+
+mixta = [1,'Hola', True, 2.5]
+print(mixta)
+
+# Una lista Vacia
+
+vacia = []
+print(vacia)
+
+# una lista de listas
+
+matriz = [[1,2,3,3],['Ana','Juan']]
+print(matriz)
+
+# Una lista de tuplas
+
+pares = [(1,2),(3,4),(5,6)]
+print(pares)
