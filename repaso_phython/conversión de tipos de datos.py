55bf4879e4959b6a14b2e036675afeb7d9c944c3
andrearendon26 <andrea20rendon@gmail.com>
2023-03-09 07:59:50 -0500

    Se crea el archivo Conversion de tipos de datos

---
+++ repaso_phython/conversión de tipos de datos.py
@@ -0,0 +1,35 @@
+# Conversion de cadena a entero
+
+cadena = "123"
+entero = int(cadena)
+print(entero)
+
+# Conversion de cadena a flotante
+
+cadena = "3.14"
+flotante = float(cadena)
+print(flotante)
+
+# Conversion de entero a cadena
+
+entero = 42
+cadena = str(entero)
+print(cadena)
+
+# Conversion de flotante a entero
+
+flotante = 3.14
+entero = int(flotante)
+print (entero)
+
+# Conversion de entero a flotante
+
+entero = 42
+flotante = float(entero)
+print(flotante)
+
+# Conversion de booleano a cadena
+
+booleano = True
+cadena = str(booleano)
+print(cadena)
