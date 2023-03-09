e9db6874ef3302475dc2b9ed9fc9e251015be595
andrearendon26 <andrea20rendon@gmail.com>
2023-03-09 06:14:08 -0500

    Tipos de datos primitivos

---
+++ repaso_phython/main.py
@@ -0,0 +1,29 @@
+# tipos de datos primitivos
+ # Numeros enteros
+
+numero1: int = 57
+print(numero1)
+
+numero2 = 24
+print(numero2)
+
+# numeros reales
+
+numero3: float = 4.9
+print(numero3)
+
+numero4 = 5.0
+print(numero4)
+
+
+# booleanos
+
+esColombiano: bool = True
+esArgentino = False
+print(esColombiano)
+
+# Caracter y cadena de caracteres
+
+mensaje = "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida \\"
+print(mensaje)
+
