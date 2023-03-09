9e7145fe0c350f666f772355b58794a68f7294b4
andrearendon26 <andrea20rendon@gmail.com>
2023-03-09 06:44:37 -0500

    Se agrega los operadores

--- repaso_phython/main.py
+++ repaso_phython/main.py
@@ -27,3 +27,51 @@ print(esColombiano)
 mensaje = "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida \\"
 print(mensaje)
 
+# Operadores
+# Aritmeticos
+
+numero5 = 9
+numero6 = 12
+suma = numero5 + numero6
+multiplicacion = numero5 * numero6
+resta = numero5 - numero6
+division = numero5 / numero6
+modulo = numero5 % numero6
+print("La suma es: ", suma)
+print("La multiplicacion es: ", multiplicacion)
+print("La resta es: ", resta)
+print("La division es: ", division)
+print("El modulo es: ", modulo)
+
+# Asignación
+
+x = 7
+y = 8
+z = 2
+print(x)
+
+# Logicos
+# and(y)
+q = 5
+print(q > 4 and q < 9)
+
+# or(ó)
+p = 4
+print(p > 5 or p < 10)
+
+# not
+print(not(p > 2 and q < 7))
+
+# Relacionales
+
+valor1 = 7
+valor2 = 9
+
+print("Igualdad:", valor1 == valor2)  # Igualdad
+print("Mayor que:", valor1 > valor2)   # Mayor que
+print("Menor que:", valor1 < valor2)   # Menor que
+print("Mayor igual que:", valor1 >= valor2) # Mayor igual que
+print("Menor igual que:", valor1 <= valor2) # Menor igual que
+print("No igual:", valor1 != valor2) # No igual
+
+
