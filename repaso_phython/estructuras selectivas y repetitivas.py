e18e551e086f119186dd21c089e597c292a20e7a
andrearendon26 <andrea20rendon@gmail.com>
2023-03-09 07:52:55 -0500

    Se crea el archivo Estructuras selectivas y repetitivas

---
+++ repaso_phython/estructuras selectivas y repetitivas.py
@@ -0,0 +1,34 @@
+# Estructura Selectiva
+# if-else
+
+edad = 18
+if edad >= 18:
+    print("Eres Mayor de edad")
+else:
+    print("Eres menor de edad")
+
+# if-elif-else
+
+dia = "lunes"
+if dia == "lunes":
+    print("Hoy es lunes")
+elif dia == "martes":
+    print("Hoy es martes")
+else:
+    print("Hoy es otro dia de la semana")
+
+# Estructura Repetitiva
+#while
+
+contador = 0
+while contador < 10:
+    print("El contador es:",contador)
+    contador +=1
+#for
+
+for i in range(5):
+    print("El valor de i es: ", i)
+
+
+
+
