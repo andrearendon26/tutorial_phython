1306e5f6646d11f9b459f6187805c66acdb9d173
andrearendon26 <andrea20rendon@gmail.com>
2023-03-09 08:26:05 -0500

    Se crea el archivo Diccionario

---
+++ repaso_phython/diccionarios.py
@@ -0,0 +1,11 @@
+# Diccionario de numeros de telefonos
+
+telefono = {'juan':24844, 'Maria':45656, 'Daniel':45567,}
+print(telefono)
+
+# Diccionario de precio de productos
+
+precios = {'manzanas': 2.900, 'bananas': 1.500,'papas': 2.400}
+print(precios)
+
+
